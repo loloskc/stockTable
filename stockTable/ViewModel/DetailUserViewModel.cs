b493d13f0d62c99ca64971305b9f05d95ac40fd0
loloskc <denishome21@gmail.com>
2024-04-10 00:27:45 +0500

    add prop in view model

--- stockTable/ViewModel/DetailUserViewModel.cs
+++ stockTable/ViewModel/DetailUserViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace stockTable.ViewModel
+﻿using stockTable.Models;
+
+namespace stockTable.ViewModel
 {
     public class DetailUserViewModel
     {
+        public User User { get; set; }
+        public string RoleName { get; set; }
     }
 }
