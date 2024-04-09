6d13dcd62bd2b44f0d63899fe9a43ec33cf241b5
loloskc <denishome21@gmail.com>
2024-04-10 00:00:50 +0500

    fixed checkbox for password

--- stockTable/wwwroot/js/site.js
+++ stockTable/wwwroot/js/site.js
@@ -5,12 +5,15 @@
 
 let checkboxPassword = document.getElementById("passwordCheckBox");
 let inputPassword = document.getElementById("passwordInput");
+let inputConfirmPassword = document.getElementById("passwordConfirm")
 
 function openPassword() {
     if (checkboxPassword.checked) {
         inputPassword.type = "text";
+        inputConfirmPassword.type = "text";
     }
     else {
         inputPassword.type = "password";
+        inputConfirmPassword.type = "password";
     }
 }
