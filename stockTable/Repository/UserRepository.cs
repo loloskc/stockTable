e23b6b955ec0d60ba55267a2237268ce51cb449d
loloskc <denishome21@gmail.com>
2024-04-09 17:56:43 +0500

    Add empty userRepository

--- stockTable/Repository/UserRepository.cs
+++ stockTable/Repository/UserRepository.cs
@@ -1,6 +1,30 @@
-﻿namespace stockTable.Repository
+﻿using stockTable.Data;
+using stockTable.Interfaces;
+using stockTable.Models;
+
+namespace stockTable.Repository
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUserByRole(string role)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
