fa11721fbb81fcb17027a2491e1befd6ef091382
loloskc <denishome21@gmail.com>
2024-04-10 04:58:45 +0500

    create new view model for eq

---
+++ stockTable/ViewModel/DetailEquipmentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using stockTable.Models;
+
+namespace stockTable.ViewModel
+{
+    public class DetailEquipmentViewModel
+    {
+        public Equipment Equipment { get; set; }
+        public int[] ImageArray { get; set; }
+    }
+}
