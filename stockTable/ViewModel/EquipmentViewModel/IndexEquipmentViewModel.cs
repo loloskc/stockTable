5e236ba30754903a48e79e41664c2371fd1a7a68
loloskc <denishome21@gmail.com>
2024-04-11 23:40:34 +0500

    create new viewModel for equipment

---
+++ stockTable/ViewModel/EquipmentViewModel/IndexEquipmentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using stockTable.Models;
+
+namespace stockTable.ViewModel.EquipmentViewModel
+{
+    public class IndexEquipmentViewModel
+    {
+        public IEnumerable<Equipment> Equipments { get; set; }
+        public string SearchField { get; set; }
+        public int StatusId { get; set; }
+    }
+}
