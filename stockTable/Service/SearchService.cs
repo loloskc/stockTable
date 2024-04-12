e10ad3b8f5c71479d7fd7aa014046ec885192806
loloskc <denishome21@gmail.com>
2024-04-12 14:26:09 +0500

    new service for search model equipment and more model

---
+++ stockTable/Service/SearchService.cs
@@ -0,0 +1,28 @@
+﻿using stockTable.Models;
+
+namespace stockTable.Service
+{
+    public class SearchService
+    {
+        public IEnumerable<Equipment> GetEquipment(IEnumerable<Equipment> equipments,string searchField)
+        {
+
+            if (!String.IsNullOrEmpty(searchField)&&equipments.Count()!=0)
+            {
+                var searchByInventoryNum = equipments.Where(c => c.InventoryNum!.Contains(searchField)).ToList();
+                var searchByModel = equipments.Where(c=>c.Model!.Contains(searchField)).ToList();
+                var searchByTypeEq = equipments.Where(c=>c.TypeEq!.Contains(searchField)).ToList();
+                var searchByIp = equipments.Where(c=>c.IPAddress!.Contains(searchField)).ToList();
+                var searchBySerialNum = equipments.Where(c=>c.SerialNum!.Contains(searchField)).ToList();
+                var searchByResponsibly = equipments.Where(c=>c.Document.Responsible!.Contains(searchField)).ToList();
+
+                var result = searchByInventoryNum.Union(searchByModel).Union(searchByTypeEq).Union(searchByIp).Union(searchBySerialNum).Union(searchByResponsibly);
+                return result;
+            }
+            else
+            {
+                return equipments;  
+            }
+        }
+    }
+}
